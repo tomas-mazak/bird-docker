267e94f0786ea474a051f8c49a1ab1290a35d822
Tomas Mazak <tomas.mazak@economia.cz>
2017-11-27 01:04:54 +0000

    Set router id to 127.0.0.1 if eth0 not configured

--- bird-wrapper.sh
+++ bird-wrapper.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-ETH0_ADDR=`ip -4 -o a | awk '/eth0/ { split($4, a, "/"); print a[1] }'`
+ROUTER_ID=`ip -4 -o a | awk '/eth0/ { split($4, a, "/"); print a[1] }'`
+if [ -z "$ROUTER_ID" ]; then
+    ROUTER_ID='127.0.0.1'
+fi
 
 # Update bird.conf
-sed -i "s/BIRD_ROUTERID/${ETH0_ADDR}/g" /etc/bird.conf
+sed -i "s/BIRD_ROUTERID/${ROUTER_ID}/g" /etc/bird.conf
 
 # Start bird
 bird
